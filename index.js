bcedd9df59e3dabd9632669814bf617a16bf3ca3
LeoBrd <leolouoscar@gmail.com>
2023-01-05 00:09:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require("discord.js");
 
 const bot = new Client({ intents: ["Guilds"] });
 console.log("Connexion au bot...");
-bot.login("MTA1NzY2Mjk0MzY4NjI5OTY0OA.G5IYrt.Idrmy-d90M2WWESaOU9hAm9YL2-nNqct7iPto4")
+bot.login("REPLACE WITH YOUR TOKEN")
     .then(() => console.log("Connecté au bot !"))
     .catch((error) => console.log("Impossible de se connecter au bot - " + error));
 
